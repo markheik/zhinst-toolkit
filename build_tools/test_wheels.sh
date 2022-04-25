ce6fa75393a193acf91e4107e8b092437c12f0b1
Markus Heikkinen <markus.heikkinen@zhinst.com>
2022-04-25 12:15:58 +0200

    version test

--- build_tools/test_wheels.sh
+++ build_tools/test_wheels.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wheelPath=$(find dist -type f -name \*.whl)
 pip install --upgrade --force-reinstall $wheelPath
-python3 -c "import zhinst.toolkit; from zhinst.toolkit import __version__; print(__version__); assert __version__ == 123"
+python3 -c "import zhinst.toolkit; from zhinst.toolkit import __version__; print(__version__); assert __version__ == $1"
